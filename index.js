01671a7198728d69df1c516b371099c534852104
deepak19kumar <deepakkushwaha695@gmail.com>
2025-08-04 18:37:09 +0530

    add

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// server.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
